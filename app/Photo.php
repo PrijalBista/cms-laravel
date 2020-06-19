0d0a8583a38e4ce6bb0af509d07cec592d8f1fe5
PrijalBista <pri1.zzal@gmail.com>
2020-06-19 23:59:36 +0545

    Add Photo model and migration (polymorphic relation setup) Photo migration has photoable_id and photoable_type any model can have relationship with Photo model.

---
+++ app/Photo.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Photo extends Model
+{
+
+    protected $fillable = ['title', 'url', 'photoable_id', 'photoable_type'];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['photo_url'];
+
+
+    // Setup polymorphic relationship allowing Feed, Post, etc (multiple models) to have photos.
+    public function photoable() {
+    	return $this->morphTo(); // will get return single model of photoable_type having id photoable_id   
+    }
+
+
+    public function getPhotoUrlAttribute() {
+    	return '/storage/' . $this->url;
+    }
+}
