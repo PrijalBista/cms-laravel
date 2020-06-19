e16b8388eed8683b9cf6520edc65a9a760be1352
PrijalBista <pri1.zzal@gmail.com>
2020-06-20 00:03:01 +0545

    Add photos() relationship method to get photos

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title', 'content'];
+
+    // polymorphic relationship with App\Photo get photos related to Post Model and certain post id.
+    public function photos() {
+		return $this->morphMany('App\Photo', 'photoable');
+    }
 }
