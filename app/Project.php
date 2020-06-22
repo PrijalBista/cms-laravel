f671dce0ae95a19cd5b55300bb0f88f9e1550754
PrijalBista <pri1.zzal@gmail.com>
2020-06-22 18:00:51 +0545

    Add HasPhoto Trait to Project Model It will add Project Model capability to add photos for each project. photos will be bind to Project Model and can be multiple, is updatable, and gets deleted automatically if project is deleted. (using events closure) also the photo files stored in the filesystem will also be deleted automatically

--- app/Project.php
+++ app/Project.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\HasPhoto;
 
 class Project extends Model
 {
+	use HasPhoto;
+
     protected $fillable = ['title', 'content'];
 }
