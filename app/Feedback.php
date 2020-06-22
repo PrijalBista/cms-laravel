bdbcb7dc458c3b5fcd66d4d99b01d2a5f36b7c31
PrijalBista <pri1.zzal@gmail.com>
2020-06-22 16:17:08 +0545

    Add hasphoto trait to Feedback Model

--- app/Feedback.php
+++ app/Feedback.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\HasPhoto;
 
 class Feedback extends Model
 {
+
+	use HasPhoto;
+
     protected $fillable = ['title', 'content'];
 }
