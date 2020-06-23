11cf734fb40868d63d617197d6321e6f559b7026
PrijalBista <pri1.zzal@gmail.com>
2020-06-23 08:19:23 +0545

    Add HasMedia trait to Share model

--- app/Share.php
+++ app/Share.php
@@ -3,10 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\HasMedia;
 
 class Share extends Model
 {
+	use HasMedia;
 
 	protected $fillable = ['title', 'content'];
-	
+
 }
