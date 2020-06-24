530f930bb9a24f5ee51b0a1a789200741f60e490
PrijalBista <pri1.zzal@gmail.com>
2020-06-24 15:53:23 +0545

    Add UserSeeder with two users admin@example.com employee@example.com

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             EmailSeeder::class,
             FeedSeeder::class,
             CoverSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		User::insert([
+			[
+				'name' => 'John Doe',
+				'email' => 'admin@example.com',
+				'role' => 'admin',
+				'email_verified_at' => now(),
+				'password' => Hash::make('abcd1234'),
+				'created_at' => now(),
+				'updated_at' => now(),
+			],
+			[
+				'name' => 'Jane Doe',
+				'email' => 'employee@example.com',
+				'role' => 'employee', // default set to employee if role not passed
+				'email_verified_at' => now(),
+				'password' => Hash::make('abcd1234'),
+				'created_at' => now(),
+				'updated_at' => now(),
+			],
+		]);
+	}
+}
